041df187ee0fca3fae4addfd1e20d2837e448dc4
Gabe Hassler <gabriel.w.hassler@gmail.com>
2022-04-12 18:08:14 -0700

    more import stuff

--- src/file_imports.jl
+++ src/file_imports.jl
@@ -6,11 +6,13 @@ const ACTIVITIES = "activities"
 function import_strava(path::String, dest_dir::String)
     mkpath(dest_dir)
     if endswith(path, ".zip")
-        import_zip(path, dest_dir)
+        import_strava_zip(path, dest_dir)
+    else
+        error("not yet implemented")
     end
 end
 
-function import_zip(path::String, dest_dir::String)
+function import_strava_zip(path::String, dest_dir::String)
     tmp_dir = mktempdir()
 
     r = ZipFile.Reader(path)
@@ -31,6 +33,59 @@ function import_zip(path::String, dest_dir::String)
     close(r)
 end
 
+function import_garmin(path::String, dest_dir::String)
+    mkpath(dest_dir)
+    if endswith(path, ".zip")
+        import_garmin_zip(path, dest_dir)
+    else
+        error("not yet implemented")
+    end
+end
+
+function import_garmin_zip(path::String, dest_dir::String)
+    tmp_dir_zip = mktempdir()
+    tmp_dir_fit = mktempdir()
+    r = ZipFile.Reader(path)
+    try
+        for file in r.files
+            bn = basename(file.name)
+            if startswith(bn, "UploadedFiles") && endswith(bn, ".zip")
+                tmp_path = joinpath(tmp_dir_zip, bn)
+                write(tmp_path, file)
+            end
+        end
+
+        for zipfile in readdir(tmp_dir_zip, join = true)
+            @show zipfile
+            r = ZipFile.Reader(zipfile)
+            for file in r.files
+                if endswith(file.name, ".fit")
+                    tmp_path = joinpath(tmp_dir_fit, basename(file.name))
+
+                    # try
+                        write(tmp_path, read(file))
+                    # catch e
+                    #     @warn "could not unzip file " * file.name
+
+                    # end
+                end
+            end
+        end
+
+        tmp_dir2 = mktempdir()
+
+
+        @show tmp_dir
+        @show tmp_dir2
+
+        cmd = Cmd(["java", "-jar", ""])
+
+
+    catch e
+        @error "Something went wrong" exception=(e, catch_backtrace())
+    end
+end
+
 function process_file(path::String, dir::String)
     bn = basename(path)
     if endswith(path, ".gz")
@@ -43,7 +98,10 @@ function process_file(path::String, dir::String)
     # cp(path, joinpath(dir, basename(path)))
 end
 
+
+activity_finder_path = joinpath(@__DIR__, "..", "artifacts", "ActivityFinder.jar")
+
 cd(@__DIR__)
-zip_path = joinpath("..", "sample_data", "export_7225196.zip")
+zip_path = joinpath("..", "sample_data", "garmin.zip")
 dest_dir = "test_activities"
-import_strava(zip_path, dest_dir)
+import_garmin(zip_path, dest_dir)
