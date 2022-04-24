24dc0b0715bde19448968e10ed3ae3baed24f71a
Gabe Hassler <gabriel.w.hassler@gmail.com>
2022-04-24 15:08:10 -0700

    new strava import

--- src/file_imports.jl
+++ src/file_imports.jl
@@ -13,24 +13,36 @@ function import_strava(path::String, dest_dir::String)
 end
 
 function import_strava_zip(path::String, dest_dir::String)
-    tmp_dir = mktempdir()
 
-    r = ZipFile.Reader(path)
-    try
-        for file in r.files
-            sp = splitpath(file.name)
-            if length(sp) == 2 && sp[1] == ACTIVITIES
-                tmp_path = joinpath(tmp_dir, sp[2])
-                write(tmp_path, read(file))
-                process_file(tmp_path, dest_dir)
-                println(file.name)
-            end
-        end
-    catch e
-        close(r)
-        @error "Could not import zip file" exception=(e, catch_backtrace())
+    bn = basename(path)
+    bn, _ = splitext(bn)
+
+
+    tmp_dir = mktempdir()
+    unzip_dir = joinpath(tmp_dir, bn)
+    unzip(path, unzip_dir)
+    activities_dir = joinpath(unzip_dir, ACTIVITIES)
+    for file in readdir(activities_dir, join=true)
+        process_file(file, dest_dir)
     end
-    close(r)
+
+
+
+    # try
+    #     for file in r.files
+    #         sp = splitpath(file.name)
+    #         if length(sp) == 2 && sp[1] == ACTIVITIES
+    #             tmp_path = joinpath(tmp_dir, sp[2])
+    #             write(tmp_path, read(file))
+    #             process_file(tmp_path, dest_dir)
+    #             println(file.name)
+    #         end
+    #     end
+    # catch e
+    #     close(r)
+    #     @error "Could not import zip file" exception=(e, catch_backtrace())
+    # end
+    # close(r)
 end
 
 function import_garmin(path::String, dest_dir::String)
@@ -42,45 +54,45 @@ function import_garmin(path::String, dest_dir::String)
     end
 end
 
-function import_garmin_zip(path::String, dest_dir::String)
-    tmp_dir_zip = mktempdir()
-    tmp_dir_fit = mktempdir()
-    r = ZipFile.Reader(path)
-    try
-        for file in r.files
-            bn = basename(file.name)
-            if startswith(bn, "UploadedFiles") && endswith(bn, ".zip")
-                tmp_path = joinpath(tmp_dir_zip, bn)
-                write(tmp_path, file)
-            end
-        end
+# function import_garmin_zip(path::String, dest_dir::String)
+#     tmp_dir_zip = mktempdir()
+#     tmp_dir_fit = mktempdir()
+#     r = ZipFile.Reader(path)
+#     try
+#         for file in r.files
+#             bn = basename(file.name)
+#             if startswith(bn, "UploadedFiles") && endswith(bn, ".zip")
+#                 tmp_path = joinpath(tmp_dir_zip, bn)
+#                 write(tmp_path, file)
+#             end
+#         end
 
-        failed = String[]
-
-
-        for zipfile in readdir(tmp_dir_zip, join = true)
-            r = ZipFile.Reader(zipfile)
-            for file in r.files
-                if endswith(file.name, ".fit")
-                    tmp_path = joinpath(tmp_dir_fit, basename(file.name))
-                    try
-                        write(tmp_path, read(file))
-                    catch
-                        push!(failed, file.name) # not sure why this works, but it prevents EOF error (and not just because of the catch)
-                    end
-                end
-            end
-        end
-
-        return failed
-
-        cmd = Cmd(["java", "-jar", ""])
+#         failed = String[]
 
 
-    catch e
-        @error "Something went wrong" exception=(e, catch_backtrace())
-    end
-end
+#         for zipfile in readdir(tmp_dir_zip, join = true)
+#             r = ZipFile.Reader(zipfile)
+#             for file in r.files
+#                 if endswith(file.name, ".fit")
+#                     tmp_path = joinpath(tmp_dir_fit, basename(file.name))
+#                     try
+#                         write(tmp_path, read(file))
+#                     catch
+#                         push!(failed, file.name) # not sure why this works, but it prevents EOF error (and not just because of the catch)
+#                     end
+#                 end
+#             end
+#         end
+
+#         return failed
+
+#         cmd = Cmd(["java", "-jar", ""])
+
+
+#     catch e
+#         @error "Something went wrong" exception=(e, catch_backtrace())
+#     end
+# end
 
 function process_file(path::String, dir::String)
     bn = basename(path)
@@ -90,18 +102,23 @@ function process_file(path::String, dir::String)
             write(joinpath(dir, bn[1:(end - 3)]), contents)
         end
         # new_path = path[1:(end - 3)]
+    else
+        @warn "cannot process file $(basename(path)). Unknown file extension"
     end
     # cp(path, joinpath(dir, basename(path)))
 end
 
-function unzip(path::String)
-    @assert endswith(path, ".zip")
-    new_dir = path[1:(end - 4)]
+
+function unzip(src::String, dest::String)
     if Sys.iswindows()
-        cmd = Cmd(["PowerShell", "-Command", "Expand-Archive", "-LiteralPath", "'$path'", "'$new_dir'"])
-        @show cmd
+        cmd = Cmd(["PowerShell", "-Command", "Expand-Archive", "-LiteralPath", "'$src'", "'$dest'"])
         run(cmd)
     else
         error("Not currently implemented for non-Windows machines.")
     end
+end
+
+
+function unzip(path::String)
+    unzip(path, splitext(path)[1])
 end
--- test/test_import.jl
+++ test/test_import.jl
@@ -1,4 +1,8 @@
 using RunMetrics
 
-data_dir = joinpath("@__DIR__", "..", "sample_data")
+data_dir = joinpath(@__DIR__, "..", "sample_data")
 
+strava_zip = joinpath(data_dir, "strava.zip")
+strava_dest = joinpath(data_dir, "stravaFiles")
+
+import_strava(strava_zip, strava_dest)
