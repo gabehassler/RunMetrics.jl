14da286bd8ac1617d434184bd6aad86cd0932f70
Gabe Hassler <gabriel.w.hassler@gmail.com>
2022-05-22 13:17:12 -0700

    test import

--- test/test_import.jl
+++ test/test_import.jl
@@ -2,11 +2,11 @@ using RunMetrics
 
 data_dir = joinpath(@__DIR__, "..", "sample_data")
 
-strava_zip = joinpath(data_dir, "strava.zip")
+strava_zip = joinpath(data_dir, "stravaSmall.zip")
 strava_dest = joinpath(data_dir, "stravaFiles")
 
-garmin_zip = joinpath(data_dir, "garmin.zip")
+garmin_zip = joinpath(data_dir, "garminSmall.zip")
 garmin_dest = joinpath(data_dir, "garminFiles")
 
-# import_strava(strava_zip, strava_dest)
+import_strava(strava_zip, strava_dest)
 import_garmin(garmin_zip, garmin_dest)
